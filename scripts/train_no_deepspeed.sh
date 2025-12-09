d62f760be10476c50b4bc30be4dbb7e71441003d
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-12-09 13:54:42 -0800

    .

--- scripts/train_no_deepspeed.sh
+++ scripts/train_no_deepspeed.sh
@@ -63,7 +63,7 @@
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --fp32 True \
+    #--fp32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
