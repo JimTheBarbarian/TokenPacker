0ee211e40e0b8b61393b1fe034c49bb2d69ad219
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-12-10 14:07:27 -0800

    no more pip

--- scripts/train_no_deepspeed.sh
+++ scripts/train_no_deepspeed.sh
@@ -38,11 +38,11 @@
 # Multi-GPU training with PyTorch DDP
 
 #!/bin/bash
-#pip install -e .
 
 
 source /ssss/Datasets/llava-pretrain/llava_real/.venv/bin/activate
 cd /home/edayag/llava/TokenPacker
+
  torchrun --nproc_per_node=4 --master_port=25001 llava/train/train_no_deepspeed.py \
     --model_name_or_path Qwen/Qwen3-0.6b \
     --version plain \
