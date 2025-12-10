564ca1237938bd144e3053d498109c7e4c37cc29
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-12-09 17:14:51 -0800

    ???

--- scripts/train_no_deepspeed.sh
+++ scripts/train_no_deepspeed.sh
@@ -36,6 +36,9 @@
 #    --report_to wandb
 
 # Multi-GPU training with PyTorch DDP
+
+
+source ../../../../ssss/Datasets/llava-pretrain/llava_real/bin/activate
  torchrun --nproc_per_node=4 --master_port=25001 llava/train/train_no_deepspeed.py \
     --model_name_or_path Qwen/Qwen3-0.6b \
     --version plain \
