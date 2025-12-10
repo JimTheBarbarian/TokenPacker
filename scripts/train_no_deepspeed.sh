a103fa3fbaca4df0bd3211726cbd701a7a2d7e59
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-12-10 13:51:57 -0800

    pip I hate you

--- scripts/train_no_deepspeed.sh
+++ scripts/train_no_deepspeed.sh
@@ -38,10 +38,11 @@
 # Multi-GPU training with PyTorch DDP
 
 #!/bin/bash
+#pip install -e .
+
 
 source /ssss/Datasets/llava-pretrain/llava_real/.venv/bin/activate
 cd /home/edayag/llava/TokenPacker
-pip install -e .
  torchrun --nproc_per_node=4 --master_port=25001 llava/train/train_no_deepspeed.py \
     --model_name_or_path Qwen/Qwen3-0.6b \
     --version plain \
