985096460f74109aaddada3791e70ba04c882cea
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-12-11 02:04:10 -0800

    siglip2 large up to bat

--- scripts/train_no_deepspeed.sh
+++ scripts/train_no_deepspeed.sh
@@ -48,7 +48,7 @@ cd /home/edayag/llava/TokenPacker
     --version plain \
     --data_path ../../../../../ssss/Datasets/llava-pretrain/blip_laion_cc_sbu_558k.json \
     --image_folder ../../../../../ssss/Datasets/llava-pretrain/llava_pretrain_558k \
-    --vision_tower google/siglip2-base-patch16-384 \
+    --vision_tower google/siglip2-large-patch16-384 \
     --hidden_size 768 \
     --mm_projector_type tokenpacker \
     --scale_factor 2 \
